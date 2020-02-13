cfccf164be5257d9e282b5858d0a2886c6e15d55
Lars Henrik Berg-Jensen <larshbj@gmail.com>
2020-02-13 23:20:26 +0100

    Updates

---
+++ precache-manifest.59efea33ee56e9875b9db973c8081b14.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4127c43adae41b5d50d887cd3557679c",
+    "url": "/bottle-detector/index.html"
+  },
+  {
+    "revision": "3ef6a6146bf0874e1d20",
+    "url": "/bottle-detector/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "74d5aaa8cbff5091ef11",
+    "url": "/bottle-detector/static/js/2.eb82f327.chunk.js"
+  },
+  {
+    "revision": "ee4799017af56988faea21766b608313",
+    "url": "/bottle-detector/static/js/2.eb82f327.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ef6a6146bf0874e1d20",
+    "url": "/bottle-detector/static/js/main.26888b36.chunk.js"
+  },
+  {
+    "revision": "818f3656c525cf1c6960",
+    "url": "/bottle-detector/static/js/runtime-main.dadde4ef.js"
+  }
+]);
