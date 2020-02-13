9091a49bb7faae2ba6bbdf4a3100af381b8596a9
Lars Henrik Berg-Jensen <larshbj@gmail.com>
2020-02-14 00:17:23 +0100

    Updates

---
+++ precache-manifest.6ffc2e33caf0f7c2f25ec9133be197f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f52a375ce601f2d98c5a7938a0870a0f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "909b8e187583e8346f4c",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "1f4a08b30f830140727c",
+    "url": "/static/js/2.40c7e1df.chunk.js"
+  },
+  {
+    "revision": "ee4799017af56988faea21766b608313",
+    "url": "/static/js/2.40c7e1df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "909b8e187583e8346f4c",
+    "url": "/static/js/main.db7d2f7f.chunk.js"
+  },
+  {
+    "revision": "dc50c8b9d5ede48a6b4c",
+    "url": "/static/js/runtime-main.3f262566.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/bottle-detector/precache-manifest.59efea33ee56e9875b9db973c8081b14.js"
+  "/precache-manifest.6ffc2e33caf0f7c2f25ec9133be197f4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/bottle-detector/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
